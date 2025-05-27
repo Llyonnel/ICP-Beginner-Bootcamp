c90f37b1149af7983fa5144077984d8ac88afd0a
Lionel Magdelyns <159193725+Llyonnel@users.noreply.github.com>
2025-05-27 18:25:49 +0000

    hello rust starter ok

--- modules/module1-fundamentals/exercises/01_hello_rust_starter.rs
+++ modules/module1-fundamentals/exercises/01_hello_rust_starter.rs
@@ -1,13 +1,26 @@
 use std::io;
+use chrono::prelude::*;
 
 fn main() {
     // TODO: 1. Prompt the user for their name
-
+    println!("What's your name?");
+    
     // TODO: 2. Read the user's input
-    let mut name = String::new();
+    let mut name: String = String::new();
+    io::stdin()
+        .read_line(&mut name)
+        .expect("Failed to read line");
+    
+    name = String:: from(name.trim());
 
+    // other solution, but change type
+    // let name: &str = name.trim(); 
+    
     // TODO: 3. Print a personalized greeting
+    println!("Hello dear {name}");
 
     // BONUS: Print the current date
+    let time: DateTime<Utc> = Utc::now();
+    println!("{time}");
     // Hint: You can use the chrono crate for this
 }
